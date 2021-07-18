80bf0232e0a8482b2c71ed7fcc6bac98cecde501
dscyrescotti <dscyrescotti@gmail.com>
2021-07-18 15:40:31 +0630

    Initial Commit

---
+++ Notas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Notas
+//
+//  Created by Dscyre Scotti on 18/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Notas/NotasApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotasApp.swift
+//  Notas
+//
+//  Created by Dscyre Scotti on 18/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct NotasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
